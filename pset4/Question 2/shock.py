7350c36b90228c83582e6170163a1b67f6c22931
lazdam <mattias.lazda@mail.mcgill.ca>
2022-03-23 12:33:22 -0400

    having issues with plots not converging :(

--- pset4/Question 2/shock.py
+++ pset4/Question 2/shock.py
@@ -13,7 +13,6 @@ Nsteps = 5000
 dt = 0.1
 dx = 2.0
 gamma = 5/3 # adiabatic index
-k = 0.01 # P = k x rho ** gamma
 
 x = np.arange(Ngrid) * dx # grid
 f1 = np.ones(Ngrid) # rho
@@ -21,11 +20,16 @@ f2 = np.zeros(Ngrid) # rho x u
 f3 = np.ones(Ngrid) # rho x e_tot
 u = np.zeros(Ngrid+1) # advective velocity (keep the 1st and last element zero)
 
+
+# Pressure 
+P = (gamma - 1)/gamma * (f3 - 0.5*f2**2/f1)
+
 # Compute sound speed squared
-cs2 = gamma * k * f1 ** (gamma - 1)
+cs2 = gamma * P / f1
 
 # Get initial mach number
-mach = u[:-1]/np.sqrt(cs2)
+u_wave = f2/f1
+mach = u_wave/np.sqrt(cs2)
 
 def advection(f, u, dt, dx):
     # calculating flux terms
@@ -36,8 +40,10 @@ def advection(f, u, dt, dx):
     return f
 
 # Apply initial Gaussian perturbation to energy 
-Amp, sigma = 1000, Ngrid/10
-f3 = f3 + Amp * np.exp(-(x - x.max()/2) ** 2 / sigma ** 2)
+Amp, sigma = 0.5, Ngrid/10
+f2 = f2 + Amp * np.exp(-(x - x.max()/2) ** 2 / sigma ** 2)
+f1 = f1 + Amp * np.exp(-(x - x.max()/2) ** 2 / sigma ** 2)
+f3 = f3 + 1000 * np.exp(-(x - x.max()/2) ** 2 / sigma ** 2)
 
 # plotting
 pl.ion()
@@ -45,15 +51,15 @@ fig, ax = pl.subplots(2,1)
 
 # Density plot
 x1, = ax[0].plot(x, f1, 'ro')
-ax[0].set_xlim([0, dx*Ngrid+1])
-ax[0].set_ylim([0.8, 2.1])
+#ax[0].set_xlim([0, dx*Ngrid+1])
+#ax[0].set_ylim([0, 10])
 ax[0].set_xlabel('x')
 ax[0].set_ylabel('Density')
 
 # Mach number plot
 x2, = ax[1].plot(x, mach, 'ro')
-ax[1].set_xlim([0, dx*Ngrid+1])
-#ax[1].set_ylim([0.8, 2.1])
+#ax[1].set_xlim([0, dx*Ngrid+1])
+ax[1].set_ylim([-10, 10])
 ax[1].set_xlabel('x')
 ax[1].set_ylabel(r'$\mathcal{M}$')
 
@@ -68,43 +74,45 @@ for ct in range(Nsteps):
     f2 = advection(f2, u, dt, dx)
 
     # Compute pressure 
-    P = k * f1 ** gamma
+    P = (gamma - 1)/gamma * (f3 - 0.5*f2**2/f1)
+
+    # Get cs2 
+    cs2 = gamma*P/f1
     
-    # Get pressure gradient 
-    grad_P = (P[2:] - P[:-2])/dx
+    # Add pressure gradient 
+    f2[1:-1] = f2[1:-1] - (dt/dx) * cs2[1:-1] * (f1[2:] - f1[:-2])
 
-    # Add source term
-    f2[1:-1] -= dt*grad_P
+    # Correct source term at boundary 
+    f2[0] = f2[0] - 0.5 * (dt/dx) * cs2[0] * (f1[1] - f1[0])
+    f2[-1] = f2[-1] - 0.5 * (dt/dx) * cs2[-1] * (f1[-1] - f1[-2])
 
-    # correct for source term at the boundary (reflective)
-    f2[0] -= (dt / dx) * (P[1] - P[0])
-    f2[-1] -= (dt / dx) * (P[-1] - P[-2])
     
     # Re-calculate advection velocities
-    u[1:-1] = 0.5 * ((f2[:-1] / f1[:-1]) + (f2[1:] / f1[1:])) # add contributions from f3
+    u[1:-1] = 0.5 * ((f2[:-1] / f1[:-1]) + (f2[1:] / f1[1:])) 
 
     # Advect energy 
     f3 = advection(f3, u, dt, dx)
 
     # Re calculate pressure
-    P = k * f1**gamma
+    P = (gamma - 1)/gamma * (f3 - 0.5*f2**2/f1)
 
     # Add source to energy term
-    f3 += P
+    f3[1:-1] = (gamma/(gamma-1))*P[1:-1] + 0.5*(f2[1:-1]**2/f1[1:-1]) 
 
-    # correct for source term at the boundary (reflective)
-    f3[0] -= 
-    f3[-1] -= 
+    #correct for source term at the boundary (reflective)
+    f3[0] = (gamma/(gamma-1))*P[0] + 0.5*(f2[0]**2/f1[0])
+    f3[-1] = (gamma/(gamma-1))*P[0] + 0.5*(f2[0]**2/f1[0])
 
     # Update pressure and sound speed
-    P = k * f1 ** gamma
-    cs2 = gamma * k * f1 ** (gamma - 1)
+    P = (gamma - 1)/gamma * (f3 - 0.5*f2**2/f1)
+    cs2 = gamma * P / f1
     
-    mach = u[0:-1]/np.sqrt(cs2)
+    u_wave = f2/f1
+    mach = u_wave/np.sqrt(cs2)
 
     # update the plot
     x1.set_ydata(f1)
     x2.set_ydata(mach)
     fig.canvas.draw()
-    pl.pause(0.001)
+    pl.pause(0.01)
 
